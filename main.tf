f355ab4ba94550ea7f9f0e755ea114f258b46f50
Simon Elliott <simon.elliott@hashicorp.com>
2025-10-30 13:12:43 +0000

    develop - vault-auth v1

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 module "vault-auth" {
-  source  = "github.com/sce81/Terraform-Vault-Provider-Auth"
+  source  = "github.com/sce81/terraform-vault-provider-auth-roles"
 
 
   jwt_backend_path      = var.jwt_backend_path
-  role_name             = var.role_name
+  role_name             = var.vault_role_name
   tfc_organization_name = data.tfe_organization.main.name
   vault_policy          = local.role_policy
   target_tfc_project    = var.target_tfc_project
-  vault_url             = "https://vault-cluster-public-vault-485f7257.cbb919d0.z1.hashicorp.cloud:8200/"
 }
 
 
